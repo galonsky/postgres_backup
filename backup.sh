203a055afa07f0625b701e7b1e4fcfa6be9a8150
Alex Galonsky <alex@galonsky.com>
2024-02-02 21:13:45 -0500

    passing database name

--- backup.sh
+++ backup.sh
@@ -6,7 +6,7 @@ DATE=`date -I`
 FILENAME="backup-$DATE.sql"
 export PGPASSWORD=${DATABASE_PASSWORD}
 
-pg_dump -h ${DATABASE_HOST} -U ${DATABASE_USER} > ${FILENAME}
+pg_dump -h ${DATABASE_HOST} -U ${DATABASE_USER} -d ${DATABASE_NAME} > ${FILENAME}
 gzip ${FILENAME}
 
 aws s3 cp ${FILENAME}.gz s3://${BUCKET_NAME}
