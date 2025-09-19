9950aa2f377b6aad2ed4460643d5150015f694c2
Alex Galonsky <alex.galonsky@carta.com>
2025-09-19 09:13:27 -0400

    adding schemas var

--- backup.sh
+++ backup.sh
@@ -6,7 +6,11 @@ DATE=`date -I`
 FILENAME="backup-$DATE.sql"
 export PGPASSWORD=${DATABASE_PASSWORD}
 
-pg_dump -h ${DATABASE_HOST} -U ${DATABASE_USER} -d ${DATABASE_NAME} > ${FILENAME}
+if [ -n "${SCHEMAS}" ]; then
+  pg_dump -h ${DATABASE_HOST} -U ${DATABASE_USER} -d ${DATABASE_NAME} -n ${SCHEMAS} > ${FILENAME}
+else
+  pg_dump -h ${DATABASE_HOST} -U ${DATABASE_USER} -d ${DATABASE_NAME} > ${FILENAME}
+fi
 gzip ${FILENAME}
 
 aws s3 cp ${FILENAME}.gz s3://${BUCKET_NAME}
